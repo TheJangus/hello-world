ad853792f1165dcdb90ea2f3fc6d9e9946461754
Jason Angus <jangus@umich.edu>
2022-09-01 01:51:46 +0200

    all the pages

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log(Hello world);
+console.log('Hello world!');
+
+let x = 10;
+console.log(x);
+
+let usersName = prompt('What is your name?');
+let message;
+
+if (usersName == 'Kassie'){
+    message = 'Hiya Teach!'
+} else if (usersName =='Ben'){
+    message = 'Hi Ben!'
+} 
+
+else {
+    message = 'Welcome to my site!';
+}
+document.write(message);
