79ccd8158fdcfa490ba380a6ec1d519438491e37
Jason Angus <jangus@umich.edu>
2022-09-01 19:05:20 +0200

    js script

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ console.log('Hello world!');
 let x = 10;
 console.log(x);
 
-let usersName = prompt('What is your name?');
+let usersName = prompt('What is your car maker?');
 let message;
 
-if (usersName == 'Kassie'){
-    message = 'Hiya Teach!'
-} else if (usersName =='Ben'){
-    message = 'Hi Ben!'
+if (usersName == 'Porsche'){
+    message = 'There is no substitute :-)'
+} else if (usersName =='porsche'){
+    message = 'There is no substitute :-)'
+} else if (usersName =='Mercedes'){
+    message = 'Right city, wrong choice'
 } 
 
 else {
-    message = 'Welcome to my site!';
+    message = 'Good choice, but Porsche is my favorite';
 }
 document.write(message);
